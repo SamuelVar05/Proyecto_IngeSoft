181bfd0711e7043890575c3a7e65882e4eb6aaf2
Juan Medina <jmedinaguerrero847@gmail.com>
2025-02-04 17:42:12 -0500

    Refactor user registration endpoint to return standardized ApiResponse with success message

--- Proyecto/backend/src/user/interface/controllers/user.controller.ts
+++ Proyecto/backend/src/user/interface/controllers/user.controller.ts
@@ -1,24 +1,26 @@
-// src/user/interface/controllers/user.controller.ts
 import { Controller, Post, Body } from '@nestjs/common';
 import { RegisterUserUseCase } from 'src/user/application/use-cases/register-user.use-case';
 import { RegisterUserDto } from '../dtos/register-user.dto';
+import { ApiResponse } from 'types/ApiResponse';
+import { RegisterUserResponseDto } from 'src/auth/application/use-cases/dtos/register-user-response.dto';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly registerUserUseCase: RegisterUserUseCase) {}
 
   @Post('register')
-  async register(@Body() body: RegisterUserDto) {
-    const token = await this.registerUserUseCase.execute(
+  async register(
+    @Body() body: RegisterUserDto,
+  ): Promise<ApiResponse<RegisterUserResponseDto>> {
+    const responseUseCase = await this.registerUserUseCase.execute(
       body.email,
       body.password,
     );
 
     return {
-      sucess: true,
-      data: {
-        token,
-      },
+      success: true,
+      data: responseUseCase,
+      message: 'User registered successfully',
     };
   }
 }
