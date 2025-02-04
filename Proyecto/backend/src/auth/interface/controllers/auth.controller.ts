659b6d2df486e2658297aa7f996385362f5ac002
Juan Medina <jmedinaguerrero847@gmail.com>
2025-02-04 17:42:26 -0500

    Refactor login endpoint to use LoginUserDto and return standardized ApiResponse

--- Proyecto/backend/src/auth/interface/controllers/auth.controller.ts
+++ Proyecto/backend/src/auth/interface/controllers/auth.controller.ts
@@ -1,6 +1,9 @@
 // src/auth/interface/controllers/auth.controller.ts
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode } from '@nestjs/common';
 import { AuthenticateUserUseCase } from 'src/auth/application/use-cases/authenticate-user.use-case';
+import { LoginUserDto } from '../dto/login-user.dto';
+import { ApiResponse } from 'types/ApiResponse';
+import { LoginUserResponseDto } from 'src/auth/application/use-cases/dtos/login-user-response.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,11 +12,15 @@ export class AuthController {
   ) {}
 
   @Post('login')
-  async login(@Body() body: { email: string; password: string }) {
-    const token = await this.authenticateUserUseCase.execute(
-      body.email,
-      body.password,
-    );
-    return { token };
+  @HttpCode(200)
+  async login(
+    @Body() { email, password }: LoginUserDto,
+  ): Promise<ApiResponse<LoginUserResponseDto>> {
+    const token = await this.authenticateUserUseCase.execute(email, password);
+    return {
+      data: token,
+      message: 'User logged in',
+      success: true,
+    };
   }
 }
