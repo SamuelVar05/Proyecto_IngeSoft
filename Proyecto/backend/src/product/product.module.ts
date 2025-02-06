cdfc9953b1a6beb5cd4e900ccee01f5b63305a15
Juan Medina <jmedinaguerrero847@gmail.com>
2025-02-05 22:33:47 -0500

    Add ProductModule with TypeORM integration for Product and Category entities

--- Proyecto/backend/src/product/product.module.ts
+++ Proyecto/backend/src/product/product.module.ts
@@ -1,15 +1,20 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { Module } from '@nestjs/common';
 import { TypeOrmProductRepository } from './infrastructure/repositories/product.repository';
+import { Product } from './domain/entities/product.entity';
+import { Category } from './domain/entities/category.entity';
+import { CreateProductUseCase } from './application/use-cases/create-product.use-case';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Product, Category])],
   providers: [
     {
       provide: 'IProductRepository',
       useClass: TypeOrmProductRepository,
     },
+    CreateProductUseCase,
   ],
   controllers: [],
-  exports: ['IUserRepository'],
 })
-export class UserModule {}
+export class ProductModule {}
