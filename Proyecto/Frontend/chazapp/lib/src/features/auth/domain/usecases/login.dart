ce775b3b106869eff73520b8942663479316f564
Samuel Vargas <sjvc0595@gmail.com>
2025-02-23 14:51:56 -0500

    Add LoginUseCase implementation for user authentication

---
+++ Proyecto/Frontend/chazapp/lib/src/features/auth/domain/usecases/login.dart
@@ -0,0 +1,20 @@
+import 'package:chazapp/src/core/network/data_state.dart';
+import 'package:chazapp/src/core/usecase/usecase.dart';
+import 'package:chazapp/src/features/auth/domain/entities/login_request_entity.dart';
+import 'package:chazapp/src/features/auth/domain/entities/user_entity.dart';
+import 'package:chazapp/src/features/auth/domain/repository/auth_repository.dart';
+
+class LoginUseCase
+    implements UseCase<DataState<UserEntity>, LoginRequestEntity> {
+  final AuthRepository _authRepository;
+
+  LoginUseCase(this._authRepository);
+
+  @override
+  Future<DataState<UserEntity>> call({LoginRequestEntity? params}) {
+    if (params == null) {
+      throw ArgumentError("Params cannot be null");
+    }
+    return _authRepository.login(params.email, params.password);
+  }
+}
