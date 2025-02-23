71ecee62703f7d2e9db3c24de2b7ec9cb8cbc16b
Samuel Vargas <sjvc0595@gmail.com>
2025-02-23 14:07:31 -0500

    Add LoginApiService for handling user login requests with Retrofit

---
+++ Proyecto/Frontend/chazapp/lib/src/features/auth/data/data_sources/remote/login_api_service.dart
@@ -0,0 +1,15 @@
+import 'package:chazapp/src/core/network/api_endpoints.dart';
+import 'package:chazapp/src/features/auth/data/models/user_model.dart';
+import 'package:chazapp/src/features/auth/data/models/login_request_model.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:dio/dio.dart';
+
+part "login_api_service.g.dart";
+
+@RestApi(baseUrl: baseUrl)
+abstract class LoginApiService {
+  factory LoginApiService(Dio dio) = _LoginApiService;
+
+  @POST(loginEndpoint)
+  Future<HttpResponse<UserModel>> login(@Body() LoginRequestModel body);
+}
