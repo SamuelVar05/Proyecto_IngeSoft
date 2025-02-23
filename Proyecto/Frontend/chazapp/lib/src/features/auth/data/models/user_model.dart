b04639b0309eabcc94ccdd5268bb363a690d65c1
Samuel Vargas <sjvc0595@gmail.com>
2025-02-23 14:08:04 -0500

    Add UserModel for user data serialization with JSON support

---
+++ Proyecto/Frontend/chazapp/lib/src/features/auth/data/models/user_model.dart
@@ -0,0 +1,14 @@
+import 'package:chazapp/src/features/auth/domain/entities/user_entity.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user_model.g.dart';
+
+@JsonSerializable()
+class UserModel extends UserEntity {
+  const UserModel({required super.token});
+
+  factory UserModel.fromJson(Map<String, dynamic> json) =>
+      _$UserModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$UserModelToJson(this);
+}
