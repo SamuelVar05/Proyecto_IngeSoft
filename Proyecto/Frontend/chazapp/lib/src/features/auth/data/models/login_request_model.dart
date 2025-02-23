4c97a29c7a20b8a5bfb5195de8fa65d2ed14d939
Samuel Vargas <sjvc0595@gmail.com>
2025-02-23 14:51:45 -0500

    Refactor LoginRequestModel to extend LoginRequestEntity for improved structure

--- Proyecto/Frontend/chazapp/lib/src/features/auth/data/models/login_request_model.dart
+++ Proyecto/Frontend/chazapp/lib/src/features/auth/data/models/login_request_model.dart
@@ -1,13 +1,11 @@
+import 'package:chazapp/src/features/auth/domain/entities/login_request_entity.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'login_request_model.g.dart';
 
 @JsonSerializable()
-class LoginRequestModel {
-  final String email;
-  final String password;
-
-  const LoginRequestModel({required this.email, required this.password});
+class LoginRequestModel extends LoginRequestEntity {
+  const LoginRequestModel({required super.email, required super.password});
 
   factory LoginRequestModel.fromJson(Map<String, dynamic> json) =>
       _$LoginRequestModelFromJson(json);
