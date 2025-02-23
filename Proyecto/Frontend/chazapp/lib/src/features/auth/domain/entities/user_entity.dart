ba59429c69cc81b1b7afdbd5f3bf7a744656d390
Samuel Vargas <sjvc0595@gmail.com>
2025-02-23 17:43:42 -0500

    Extend UserEntity to implement Equatable for improved equality comparison

--- Proyecto/Frontend/chazapp/lib/src/features/auth/domain/entities/user_entity.dart
+++ Proyecto/Frontend/chazapp/lib/src/features/auth/domain/entities/user_entity.dart
@@ -1,5 +1,10 @@
-class UserEntity {
+import 'package:equatable/equatable.dart';
+
+class UserEntity extends Equatable {
   final String token;
 
   const UserEntity({required this.token});
+  
+  @override
+  List<Object?> get props => [token];
 }
