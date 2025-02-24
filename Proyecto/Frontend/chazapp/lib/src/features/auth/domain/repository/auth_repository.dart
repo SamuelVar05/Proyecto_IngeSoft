baf59ab1df8596f2474e2358d04a1eee721ae650
Samuel Vargas <sjvc0595@gmail.com>
2025-02-24 18:32:06 -0500

    Add register method to AuthRepository for user registration

--- Proyecto/Frontend/chazapp/lib/src/features/auth/domain/repository/auth_repository.dart
+++ Proyecto/Frontend/chazapp/lib/src/features/auth/domain/repository/auth_repository.dart
@@ -1,6 +1,9 @@
 import 'package:chazapp/src/core/network/data_state.dart';
+import 'package:chazapp/src/features/auth/domain/entities/register_entity.dart';
 import 'package:chazapp/src/features/auth/domain/entities/user_entity.dart';
 
 abstract class AuthRepository {
   Future<DataState<UserEntity>> login(String email, String password);
+
+  Future<DataState<RegisterEntity>> register(String email, String password);
 }
