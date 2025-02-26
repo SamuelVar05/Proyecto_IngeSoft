5fcd6243fc49d955ca30e0fa29c656219a511de6
Juan Medina <jmedinaguerrero847@gmail.com>
2025-02-25 19:56:35 -0500

    Update ProductModule to include Chaza entity and ProductController

--- Proyecto/backend/src/product/product.module.ts
+++ Proyecto/backend/src/product/product.module.ts
@@ -3,11 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { TypeOrmProductRepository } from './infrastructure/repositories/product.repository';
 import { Product } from './domain/entities/product.entity';
-import { Category } from './domain/entities/category.entity';
+
 import { CreateProductUseCase } from './application/use-cases/create-product.use-case';
+import { Chaza } from 'src/chaza/domain/entities/chaza.entity';
+import { ProductController } from './interface/controller/product.controller';
+import { Category } from 'src/category/domain/entites/category.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product, Category])],
+  imports: [TypeOrmModule.forFeature([Product, Category, Chaza])],
   providers: [
     {
       provide: 'IProductRepository',
@@ -15,6 +18,6 @@ import { CreateProductUseCase } from './application/use-cases/create-product.use
     },
     CreateProductUseCase,
   ],
-  controllers: [],
+  controllers: [ProductController],
 })
 export class ProductModule {}
