2afa1d8d22b907ccc2643aeb6de4a2c252c8e6ea
Juan Medina <jmedinaguerrero847@gmail.com>
2025-02-25 20:00:50 -0500

    Add validation rules for name and description in CreateCategoryDto

--- Proyecto/backend/src/category/interface/dtos/createCategory.dto.ts
+++ Proyecto/backend/src/category/interface/dtos/createCategory.dto.ts
@@ -1,5 +1,13 @@
+import { IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreateCategoryDto {
+  @IsString()
+  @MinLength(3)
+  @MaxLength(30)
   name: string;
 
+  @IsString()
+  @MinLength(3)
+  @MaxLength(30)
   description: string;
 }
