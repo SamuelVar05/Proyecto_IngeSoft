07733d03dfa7a24a7df08b2a6aea6eb639918c7a
Samuel Vargas <sjvc0595@gmail.com>
2025-02-28 18:30:04 -0500

    Extend ProfileEntity to implement Equatable for improved equality comparison

--- Proyecto/Frontend/chazapp/lib/src/features/profile/domain/enitites/profile_entity.dart
+++ Proyecto/Frontend/chazapp/lib/src/features/profile/domain/enitites/profile_entity.dart
@@ -1,7 +1,12 @@
-class ProfileEntity {
+import 'package:equatable/equatable.dart';
+
+class ProfileEntity extends Equatable {
   final String email;
 
   const ProfileEntity({
     required this.email,
   });
+
+  @override
+  List<Object?> get props => [email];
 }
