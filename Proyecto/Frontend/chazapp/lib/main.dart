e5f559fa38e800ef49b2e72eb51b945a0692a516
Samuel Vargas <sjvc0595@gmail.com>
2025-02-28 20:33:23 -0500

    Remove ProfileBloc provider from MyApp for unused dependency cleanup

--- Proyecto/Frontend/chazapp/lib/main.dart
+++ Proyecto/Frontend/chazapp/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:chazapp/injection_container.dart';
 import 'package:chazapp/src/features/auth/presentation/bloc/login/login_bloc.dart';
 import 'package:chazapp/src/features/auth/presentation/bloc/register/register_bloc.dart';
-import 'package:chazapp/src/features/profile/presentation/bloc/profile/profile_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'src/config/router/app_router.dart';
@@ -21,7 +20,6 @@ class MyApp extends StatelessWidget {
       providers: [
         BlocProvider(create: (_) => sl<LoginBloc>()),
         BlocProvider(create: (_) => sl<RegisterBloc>()),
-        BlocProvider(create: (_) => sl<ProfileBloc>()),
       ],
       child: MaterialApp.router(
         routerConfig: router,
