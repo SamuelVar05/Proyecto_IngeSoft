3293bb8da6d873cf5240e11c187c22ea5742ae50
Samuel Vargas <sjvc0595@gmail.com>
2025-03-01 11:45:33 -0500

    Add getChazas method to ProfileRepository and implement in ProfileRepositoryImpl for fetching user chazas

--- Proyecto/Frontend/chazapp/lib/src/features/profile/data/repository/profile_repository_impl.dart
+++ Proyecto/Frontend/chazapp/lib/src/features/profile/data/repository/profile_repository_impl.dart
@@ -2,8 +2,8 @@ import 'dart:io';
 
 import 'package:chazapp/src/core/network/data_state.dart';
 import 'package:chazapp/src/features/profile/data/data_sources/remote/profile_api_service.dart';
+import 'package:chazapp/src/features/profile/data/models/chaza_model.dart';
 import 'package:chazapp/src/features/profile/data/models/profile_model.dart';
-import 'package:chazapp/src/features/profile/domain/enitites/profile_entity.dart';
 import 'package:chazapp/src/features/profile/domain/repository/profile_repository.dart';
 import 'package:dio/dio.dart';
 
@@ -14,27 +14,27 @@ class ProfileRepositoryImpl implements ProfileRepository {
   ProfileRepositoryImpl(this._profileApiService);
 
   @override
-  Future<DataState<ProfileEntity>> getProfile(String token) async {
+  Future<DataState<ProfileModel>> getProfile(String token) async {
     // Se realiza la petición al servidor
     final httpResponse = await _profileApiService.getProfile("Bearer $token");
 
-    try{
+    try {
       // Si la petición fue exitosa
-    if (httpResponse.response.statusCode == HttpStatus.ok) {
-      ProfileModel profile = httpResponse.data.data!;
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        ProfileModel profile = httpResponse.data.data!;
 
-      return DataSuccess(profile);
-    } else {
-      return DataFailed(
-        DioException(
-          error: "Error ${httpResponse.response.statusCode}",
-          message: "Error en la petición",
-          response: httpResponse.response,
-          type: DioExceptionType.badResponse,
-          requestOptions: httpResponse.response.requestOptions,
-        ),
-      );
-    }
+        return DataSuccess(profile);
+      } else {
+        return DataFailed(
+          DioException(
+            error: "Error ${httpResponse.response.statusCode}",
+            message: "Error en la petición",
+            response: httpResponse.response,
+            type: DioExceptionType.badResponse,
+            requestOptions: httpResponse.response.requestOptions,
+          ),
+        );
+      }
     } on DioException catch (e) {
       return DataFailed(
         DioException(
@@ -61,6 +61,60 @@ class ProfileRepositoryImpl implements ProfileRepository {
         type: DioExceptionType.unknown,
       ));
     }
+  }
+
+  @override
+  Future<DataState<List<ChazaModel>>> getChazas(
+      String token, String userId) async {
     
+
+    try {
+      final httpResponse =
+        await _profileApiService.getChazas("Bearer $token", userId);
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        List<ChazaModel> chazas = httpResponse.data.data!;
+
+        return DataSuccess(chazas);
+      } else {
+        return DataFailed(
+          DioException(
+            error: "Error ${httpResponse.response.statusCode}",
+            message: "Error en la petición",
+            response: httpResponse.response,
+            type: DioExceptionType.badResponse,
+            requestOptions: httpResponse.response.requestOptions,
+          ),
+        );
+      }
+    } on DioException catch (e) {
+      print("Hola");
+      if (e.response?.statusCode == HttpStatus.notFound) {
+        print("hola. Estoy en la excepción");
+        return const DataSuccess(
+            []); // Si no hay chazas, se retorna una lista vacía (no es un error
+      }
+      return DataFailed(
+        DioException(
+          error: "Error ${e.response?.statusCode}",
+          message: "Error en la petición",
+          response: e.response,
+          type: DioExceptionType.badResponse,
+          requestOptions: e.requestOptions,
+        ),
+      );
+    } on SocketException {
+      return DataFailed(DioException(
+        error: "No hay conexión a Internet.",
+        requestOptions:
+            RequestOptions(path: ''), // Se requiere un `RequestOptions`
+        type: DioExceptionType.connectionError,
+      ));
+    } catch (e) {
+      return DataFailed(DioException(
+        error: "Error inesperado: ${e.toString()}",
+        requestOptions: RequestOptions(path: ''),
+        type: DioExceptionType.unknown,
+      ));
+    }
   }
 }
--- Proyecto/Frontend/chazapp/lib/src/features/profile/domain/repository/profile_repository.dart
+++ Proyecto/Frontend/chazapp/lib/src/features/profile/domain/repository/profile_repository.dart
@@ -1,6 +1,8 @@
 import 'package:chazapp/src/core/network/data_state.dart';
+import 'package:chazapp/src/features/profile/domain/enitites/chaza_entity.dart';
 import 'package:chazapp/src/features/profile/domain/enitites/profile_entity.dart';
 
 abstract class ProfileRepository {
   Future<DataState<ProfileEntity>> getProfile(String token);
+  Future<DataState<List<ChazaEntity>>> getChazas(String token, String userId);
 }
