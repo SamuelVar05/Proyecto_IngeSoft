ba0b6b65de6c21b96a28e27aaa9a3b6e64a05ea6
Samuel Vargas <sjvc0595@gmail.com>
2025-03-01 21:40:35 -0500

    Implement ChazaRepository with createChaza method and update use case to match new parameters

---
+++ Proyecto/Frontend/chazapp/lib/src/features/chaza_creation/data/repository/chaza_repository_impl.dart
@@ -0,0 +1,80 @@
+
+import 'dart:io';
+
+import 'package:chazapp/src/core/network/data_state.dart';
+import 'package:chazapp/src/features/chaza_creation/data/data_sources/remote/chaza_creation_service.dart';
+import 'package:chazapp/src/features/chaza_creation/data/models/chaza_request_model.dart';
+import 'package:chazapp/src/features/chaza_creation/domain/repository/chaza_repository.dart';
+import 'package:dio/dio.dart';
+
+class ChazaRepositoryImpl implements ChazaRepository {
+  final ChazaCreationService _chazaCreationService;
+
+  ChazaRepositoryImpl(this._chazaCreationService);
+
+  @override
+  Future<DataState<void>> createChaza({
+    required idUsuario,
+    required nombre,
+    required descripcion,
+    ubicacion,
+    idPhoto,
+  }) async {
+    try {
+      final chazaRequest = ChazaRequestModel(
+        idUsuario: idUsuario,
+        nombre: nombre,
+        descripcion: descripcion,
+        ubicacion: ubicacion,
+        idPhoto: idPhoto,
+      );
+      final httpResponse =
+          await _chazaCreationService.createChaza(chazaRequest);
+
+      if (httpResponse.response.statusCode == HttpStatus.created) {
+        return const DataSuccess(null);
+      } else {
+        return DataFailed(DioException(
+          error: "Error ${httpResponse.response.statusCode}",
+          message: "Error en la petición",
+          response: httpResponse.response,
+          type: DioExceptionType.badResponse,
+          requestOptions: httpResponse.response.requestOptions,
+        ));
+      }
+    } on DioException catch (e) {
+      if (e.response?.statusCode == HttpStatus.badRequest) {
+        return DataFailed(DioException(
+          error: "Error ${e.response?.statusCode}",
+          message: "Campos inválidos",
+          response: e.response,
+          type: DioExceptionType.badResponse,
+          requestOptions: e.requestOptions,
+        ));
+      }
+
+      return DataFailed(DioException(
+        error: "Error ${e.response?.statusCode}",
+        message: "Error en la petición",
+        response: e.response,
+        type: DioExceptionType.badResponse,
+        requestOptions: e.requestOptions,
+      ));
+    } on SocketException {
+      // Si no hay conexión a Internet
+      return DataFailed(DioException(
+        error: "No hay conexión a Internet.",
+        requestOptions:
+            RequestOptions(path: ''), // Se requiere un `RequestOptions`
+        type: DioExceptionType.connectionError,
+      ));
+    } catch (e) {
+      // Si ocurre un error inesperado
+      return DataFailed(DioException(
+        error: "Error inesperado: ${e.toString()}",
+        requestOptions: RequestOptions(path: ''),
+        type: DioExceptionType.unknown,
+      ));
+    }
+  }
+}
--- Proyecto/Frontend/chazapp/lib/src/features/chaza_creation/domain/repository/chaza_repository.dart
+++ Proyecto/Frontend/chazapp/lib/src/features/chaza_creation/domain/repository/chaza_repository.dart
@@ -1,6 +1,11 @@
 import 'package:chazapp/src/core/network/data_state.dart';
-import 'package:chazapp/src/features/chaza_creation/domain/entity/chaza_request_entity.dart';
 
 abstract class ChazaRepository {
-  Future<DataState<void>> createChaza(ChazaRequestEntity chazaRequest);
+  Future<DataState<void>> createChaza({
+    required idUsuario,
+    required nombre,
+    required descripcion,
+    ubicacion,
+    idPhoto,
+  });
 }
