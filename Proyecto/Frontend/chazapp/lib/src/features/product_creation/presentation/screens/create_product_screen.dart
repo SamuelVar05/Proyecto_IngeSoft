f490136beb8f6d58038d22c47c6482c95025ae29
Samuel Vargas <sjvc0595@gmail.com>
2025-03-04 03:08:44 -0500

    Add CreateProductScreen for product creation interface

---
+++ Proyecto/Frontend/chazapp/lib/src/features/product_creation/presentation/screens/create_product_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/widgets.dart';
+
+class CreateProductScreen extends StatefulWidget {
+  const CreateProductScreen({super.key});
+
+  @override
+  State<CreateProductScreen> createState() => _CreateProductScreenState();
+}
+
+class _CreateProductScreenState extends State<CreateProductScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _priceController = TextEditingController();
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
