59b9b1540e0478f10ff128c0bae6f8715423dbcb
Samuel Vargas <sjvc0595@gmail.com>
2025-03-04 02:28:27 -0500

    Add ProductsBloc to main application for product creation flow

--- Proyecto/Frontend/chazapp/lib/main.dart
+++ Proyecto/Frontend/chazapp/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:chazapp/src/features/auth/presentation/bloc/login/login_event.da
 import 'package:chazapp/src/features/auth/presentation/bloc/register/register_bloc.dart';
 import 'package:chazapp/src/features/chaza_creation/presentation/bloc/chaza_creation/chaza_creation_bloc.dart';
 import 'package:chazapp/src/features/home/presentation/bloc/productos/productos_bloc.dart';
+import 'package:chazapp/src/features/product_creation/presentation/bloc/products/products_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'src/config/router/app_router.dart';
@@ -25,6 +26,7 @@ class MyApp extends StatelessWidget {
         BlocProvider(create: (_) => sl<RegisterBloc>()),
         BlocProvider(create: (_) => sl<ProductosBloc>()),
         BlocProvider(create: (_) => sl<ChazaCreationBloc>()),
+        BlocProvider(create: (_) => sl<ProductsBloc>()),
       ],
       child: MaterialApp.router(
         routerConfig: router,
